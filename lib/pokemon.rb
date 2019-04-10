304c7c28cc814bd674533038aece78036f5280b1
Sachin Mahajan <sachinm78@gmail.com>
2019-04-10 19:37:54 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -3,6 +3,17 @@ class Pokemon
   @@all = []
   
   def initialize(:id, :name: :type, :db)
+    @id = :id
+    @name = :name
+    @type = :type
+    @db = :db
+  end
+  
+  def self.save
     
+  end
+  
+  def self.find(id)
+  
   end
 end
